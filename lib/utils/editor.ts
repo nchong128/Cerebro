842dbca64369ae51b9729633c3f1c6dea4a5a57f
Nick Chong <nchong128@gmail.com>
2024-03-24 12:19:35 +1100

    Fix bug where chat would start at cursor

--- lib/utils/editor.ts
+++ lib/utils/editor.ts
@@ -2,13 +2,14 @@ import { Editor, EditorPosition } from 'obsidian';
 
 export const completeUserResponse = (editor: Editor, headingPrefix: string): EditorPosition => {
 	/**
-	 * 1. Places divider
-	 * 2. Completes the user's response by placing the assistant's header
-	 * 3. Moves cursor to end of line
+	 * 1. Moves cursor to end of line
+	 * 2. Places divider
+	 * 3. Completes the user's response by placing the assistant's header
 	 */
+	moveCursorToEndOfFile(editor);
 	const newLine = `\n\n<hr class="__chatgpt_plugin">\n\n${headingPrefix}role::assistant\n\n`;
 	editor.replaceRange(newLine, editor.getCursor());
-	return moveCursorToEol(editor, newLine);
+	return moveCursorToEndOfLine(editor, newLine);
 };
 
 export const completeAssistantResponse = (
@@ -22,7 +23,7 @@ export const completeAssistantResponse = (
 	 */
 	const newLine = `\n\n<hr class="__chatgpt_plugin">\n\n${headingPrefix}role::user\n\n`;
 	editor.replaceRange(newLine, editor.getCursor());
-	return moveCursorToEol(editor, newLine);
+	return moveCursorToEndOfLine(editor, newLine);
 };
 
 export const appendNonStreamingMessage = (
@@ -37,11 +38,32 @@ export const appendNonStreamingMessage = (
 
 	// const newLine = `\n\n<hr class="__chatgpt_plugin">\n\n${headingPrefix}role::assistant\n\n${message}\n\n`;
 	editor.replaceRange(message, editor.getCursor());
-	return moveCursorToEol(editor, message);
+	return moveCursorToEndOfLine(editor, message);
 };
 
-export const moveCursorToEol = (editor: Editor, change: string): EditorPosition => {
-	// Move cursor to end of line
+
+export const moveCursorToEndOfFile = (editor: Editor) => {
+	try {
+		// Get length of file
+		const length = editor.lastLine();
+
+		// Move cursor to end of file https://davidwalsh.name/codemirror-set-focus-line
+		const newCursor = {
+			line: length + 1,
+			ch: 0,
+		};
+		editor.setCursor(newCursor);
+
+		return newCursor;
+	} catch (err) {
+		throw new Error('Error moving cursor to end of file' + err);
+	}
+}
+
+
+
+export const moveCursorToEndOfLine = (editor: Editor, change: string): EditorPosition => {
+	// Moves cursor to end of line
 	const cursor = editor.getCursor();
 	const newCursor: EditorPosition = {
 		line: cursor.line,
