438ede5e89759589d90994599e96c5d979ed6d82
Nick Chong <nchong128@gmail.com>
2024-11-21 17:18:11 +1100

    Extract logic to retrieving frontmatter from settings to utils

--- lib/types.ts
+++ lib/types.ts
@@ -3,29 +3,6 @@ import OpenAI from 'openai';
 
 export type LLM = 'OpenAI' | 'Anthropic';
 
-type OpenAISettings = {
-	apiKey: string;
-	defaultChatFrontmatter: string;
-};
-
-type AnthropicSettings = {
-	apiKey: string;
-	defaultChatFrontmatter: string;
-};
-
-export interface CerebroSettings {
-	defaultLLM: LLM;
-	openAISettings: OpenAISettings;
-	anthropicSettings: AnthropicSettings;
-	stream: boolean;
-	chatTemplateFolder: string;
-	chatFolder: string;
-	autoInferTitle: boolean;
-	dateFormat: string;
-	headingLevel: number;
-	inferTitleLanguage: string;
-}
-
 export type ChatFrontmatter = Omit<OpenAI.ChatCompletionCreateParams, 'messages'> & {
 	title: string;
 	tags: FrontMatterCache;
