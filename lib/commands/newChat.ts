61fb155ae82c6f4f021864a39a6cbb7949efac7b
Nick Chong <nchong128@gmail.com>
2025-01-30 16:47:02 +1300

    No longer require an active file to start a new chat with Cerebro

--- lib/commands/newChat.ts
+++ lib/commands/newChat.ts
@@ -6,23 +6,30 @@ import { logger } from '../logger';
 import { createNewChatFile, openInMainEditor } from './chatCreation';
 
 export const createNewChatCommand = (plugin: Cerebro): Command => ({
-	id: 'cerebro-create-new-chat',
-	name: 'Create new chat',
-	icon: 'message-square-plus',
-	editorCallback: async (editor: Editor, view: MarkdownView) => {
-		const chatInterface = new ChatInterface(plugin.settings, editor, view);
+    id: 'cerebro-create-new-chat',
+    name: 'Create new chat',
+    icon: 'message-square-plus',
+    callback: async () => {
+        try {
+            const activeView = plugin.app.workspace.getActiveViewOfType(MarkdownView);
+            const selectedText = activeView?.editor?.getSelection() || '';
 
-		try {
-			const selectedText = editor.getSelection();
-			const newFile = await createNewChatFile(plugin, selectedText);
-			if (!newFile) return;
-			openInMainEditor(plugin, newFile, chatInterface);
-		} catch (e) {
-			logger.error(`[Cerebro] Error when creating new chat`, e);
-			new Notice(
-				`[Cerebro] Error while creating new chat. See console for more details. ${e.message}`,
-				ERROR_NOTICE_TIMEOUT_MILLISECONDS,
-			);
-		}
-	},
+            const newFile = await createNewChatFile(plugin, selectedText);
+            if (!newFile) return;
+
+            const newView: any = await plugin.app.workspace.getLeaf().openFile(newFile);
+
+            if (!(newView instanceof MarkdownView)) return;
+
+            const chatInterface = new ChatInterface(plugin.settings, newView.editor, newView);
+
+            openInMainEditor(plugin, newFile, chatInterface);
+        } catch (e) {
+            logger.error(`[Cerebro] Error when creating new chat`, e);
+            new Notice(
+                `[Cerebro] Error while creating new chat. See console for more details. ${e.message}`,
+                ERROR_NOTICE_TIMEOUT_MILLISECONDS,
+            );
+        }
+    },
 });
